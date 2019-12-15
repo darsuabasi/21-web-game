7827d423ccad5f5b03375f078663f68043b3d77a
darsuabasi <uduakabasiabasiurua@pursuit.org>
2019-12-15 14:13:09 -0500

    displaying human and dealer

--- game.js
+++ game.js
@@ -4,10 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
     let stayButton = document.querySelector("#stayButton")
     let displayCards = document.querySelector("#displayCards")
     let deckId;
+    let humanPlayer = document.querySelector("#humanPlayer")
+    let dealer = document.querySelector("#dealer")
 
-    const drawCard = async (number) => {
-        await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${number}`)
-    }
+    // const drawCard = async (number) => {
+    //     await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=${number}`
+    //     )
+    // }
 
     
     const startGame = async () => {
@@ -15,12 +18,12 @@ document.addEventListener("DOMContentLoaded", () => {
             let shuffledCards = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
             deckId = shuffledCards.data.deck_id
             
-            let drawTwoCards = drawCard(2)
+            let drawTwoCards = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
             
-            drawTwoCards.data.cards.forEach(el=>{
+            drawTwoCards.data.cards.forEach(el => {
                 let image = document.createElement("img")
                 image.src = el.image
-                displayCards.appendChild(image)
+                humanPlayer.appendChild(image)
             })
         }catch (err) {
             console.log(err)
@@ -28,22 +31,34 @@ document.addEventListener("DOMContentLoaded", () => {
     } 
 
     const hitHumanPlayer = async () => {
-        let drawOneCard = 
+        let drawOneCard = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+        debugger
+        drawOneCard.data.cards.forEach(el => {
+            let image = document.createElement("img")
+            image.src = el.image
+            humanPlayer.appendChild(image)
+        })
     }
 
     const hitDealer = async () => {
         let drawThreeCards = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=3`)
+        drawThreeCards.data.cards.forEach(el => {
+            let image = document.createElement("img")
+            image.src = el.image
+            dealer.appendChild(image)
+        })
     }
     
     startGameButton.addEventListener("click", () => {
                     startGame();
                 })
     
-                stayButton.addEventListener("click", () => {
-    
+    stayButton.addEventListener("click", () => {
+                hitDealer();
                 })
-                hitButton.addEventListener("click", () => {
-    
+                
+    hitButton.addEventListener("click", () => {
+                hitHumanPlayer();
                 })
     
     })
